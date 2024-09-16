c51116d63b917fa7a0bf52a6d9214316866aa9ab
Bhushan <bhussh31@gmail.com>
2024-09-16 13:19:11 +0530

    Changes on 16 sept

--- Hello.java
+++ Hello.java
@@ -3,9 +3,6 @@ package com.demo;
 public class Hello {
 
 	public static void main(String[] args) {
-System.out.println("Hi");
-System.out.println("Vipin");
-		System.out.println("Bhushan Gadge");
+	System.out.println("Hi");
 	}
-
 }
